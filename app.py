68fc60f286210bc6eef4ce003ee223f48376d984
ripusudan021 <mishraripusudan021@gmail.com>
2026-07-07 19:30:12 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+import requests
+from config import NEWS_API_KEY
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    query = request.args.get("query", "latest")
+
+    url = f"https://newsapi.org/v2/everything?q={query}&apiKey={NEWS_API_KEY}"
+
+    response = requests.get(url)
+    news_data = response.json()
+
+    articles = news_data.get("articles", [])
+    filter_articles = [article for article in articles if article.get("description")]
+
+    top_article = None
+    rest_articles = []
+
+    if filter_articles:
+        top_article = filter_articles[0]
+        rest_articles = filter_articles[1:]
+
+    return render_template(
+        "index.html",
+        articles=rest_articles,
+        query=query,
+        top_article=top_article
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,1 @@
+NEWS_API_KEY  = "00365537f5854aabbc31c7715af7dbb8"
